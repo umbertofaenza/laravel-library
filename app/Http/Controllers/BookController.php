87d39d402e292b47299968bace17fd51d265b65c
umbertofaenza <umberto.faenza@gmail.com>
2023-10-20 18:01:25 +0200

    edit comments in controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,28 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function index()
     {
-        
-    
+
+
         $title = "Books";
         $books = Book::all();
 
         return view('books.index', compact('title', 'books'));
-    
+
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -36,7 +37,7 @@ class BookController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -51,7 +52,7 @@ class BookController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function show(Book $book)
     {
@@ -62,7 +63,7 @@ class BookController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function edit(Book $book)
     {
@@ -74,7 +75,7 @@ class BookController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function update(Request $request, Book $book)
     {
@@ -87,7 +88,7 @@ class BookController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
