078bd801cd279949e59d98e5fe8d00a3165627bb
umbertofaenza <umberto.faenza@gmail.com>
2023-10-24 16:22:58 +0200

    edit web.php and BookController

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Http\Controllers\Controller;
 
 class BookController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        Route::resource('books', BookController::class);
+
+    });
+
+require __DIR__ . '/auth.php';
