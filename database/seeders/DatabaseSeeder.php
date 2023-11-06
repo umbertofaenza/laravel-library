8a9f8fc1f549bc14d9c79ae4d1d913aab0ff3cbb
umbertofaenza <umberto.faenza@gmail.com>
2023-11-06 16:03:50 +0100

    add TypeSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            GenreSeeder::class,
             UserSeeder::class,
+            GenreSeeder::class,
+            TypeSeeder::class,
             BooksTableSeeder::class,
         ]);
     }
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $labels = ["Paperback", "Audio-book", "E-book", "Magazine"];
+
+        foreach ($labels as $label) {
+            $type = new Type();
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
+    }
+}
